d6f9d9469bf87eedbac346fe6832af0cc45af059
Shaik Ansar Basha <AnsarBasha.Shaik@fujitsu.com>
2025-06-04 17:11:32 +0530

    updatedfiles9

--- kube_secret.sh
+++ kube_secret.sh
@@ -1,3 +1,10 @@
 # !/bin/bash
 
-kubectl create secret docker-registry notescred --docker-username=$DOCKER_USERNAME --docker-password=$DOCKER_PASSWORD --docker-email=sabasha1485@gmail.com --namespace=notes-app
+echo "Username: $DOCKER_USERNAME"
+echo "Password: $DOCKER_PASSWORD"
+
+kubectl create secret docker-registry notescred \
+  --docker-username=$DOCKER_USERNAME \
+  --docker-password=$DOCKER_PASSWORD \
+  --docker-email=sabasha1485@gmail.com \
+  --namespace=notes-app
