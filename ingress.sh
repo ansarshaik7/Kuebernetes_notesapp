cf89f81739bf10fc227a090a44fb68efc6fbadf3
Shaik Ansar Basha <AnsarBasha.Shaik@fujitsu.com>
2025-06-04 20:32:08 +0530

    updatedfiles23

--- ingress.sh
+++ ingress.sh
@@ -1,11 +1,11 @@
 ﻿#!/bin/bash
 
 # Create ingress folder if it doesn't exist
-if [ -d "$WORKSPACE/ingress" ]; then
+if [ -d "ingress" ]; then
     echo "The ingress folder is present"
 else
     echo "The ingress folder is not present... creating the folder"
-    mkdir -p "$WORKSPACE/ingress"
+    mkdir -p ingress
     if [ $? -eq 0 ]; then
         echo "The ingress folder was created successfully"
     else
@@ -15,8 +15,8 @@ else
 fi
 
 # Copy ingress.yaml
-cp ingress.yaml "$WORKSPACE/ingress"
-cd "$WORKSPACE/ingress" || exit 1
+cp ingress.yaml ingress
+cd ingress
 
 # Install nginx ingress controller
 kubectl apply -f https://kind.sigs.k8s.io/examples/ingress/deploy-ingress-nginx.yaml
